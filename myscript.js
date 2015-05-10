22efd29ab0e987666e8b33ac9db8001daa50047f
Eelco Dijkstra <eelco.j.dijkstra@gmail.com>
2015-05-10 16:49:08 +0200

    Add: myscript

---
+++ myscript.js
@@ -0,0 +1,27 @@
+/*global
+    alert, confirm, console, Debug, opera, prompt, WSH
+*/
+
+alert("hi there - les 2");
+
+var inTextElt = document.getElementById("inText");
+
+function textHandler() {
+  var elt = document.getElementById("inText");
+  alert("input changed: " + elt.value);
+  elt.value = "OK";
+}
+
+inTextElt.onchange = textHandler;
+
+var formElt = document.getElementById("myForm");
+
+function radioHandler() {
+  var sugar = formElt.elements["sugar"].checked;
+  var milk = formElt.elements["milk"].checked;
+  alert("pizza selection: " + formElt.elements["pizza"].value + "\n" +
+        "coffee with sugar: " + sugar + "\n" +
+         "coffee with milk: " + milk);
+}
+
+formElt.onchange = radioHandler;
